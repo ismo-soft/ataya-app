b47ac7f63332dd55a8ae6f0b95faedc615ce8e7e
IsmailKattan <ismail.kattan@ogr.sakarya.edu.tr>
2025-03-19 23:15:11 +0300

    debug endpoints

--- back-end/services/company/src/main/java/com/ataya/company/controller/DebugController.java
+++ back-end/services/company/src/main/java/com/ataya/company/controller/DebugController.java
@@ -2,10 +2,13 @@ package com.ataya.company.controller;
 
 import com.ataya.company.dto.company.CompanyInfoResponse;
 import com.ataya.company.dto.store.response.StoreInfoResponse;
+import com.ataya.company.dto.worker.response.WorkerInfoResponse;
+import com.ataya.company.enums.Role;
 import com.ataya.company.model.Worker;
 import com.ataya.company.service.AuthService;
 import com.ataya.company.service.CompanyService;
 import com.ataya.company.service.StoreService;
+import com.ataya.company.service.WorkerService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,11 +25,13 @@ public class DebugController {
     private final AuthService authService;
     private final CompanyService companyService;
     private final StoreService storeService;
+    private final WorkerService workerService;
 
-    public DebugController(AuthService authService, CompanyService companyService, StoreService storeService) {
+    public DebugController(AuthService authService, CompanyService companyService, StoreService storeService, WorkerService workerService) {
         this.authService = authService;
         this.companyService = companyService;
         this.storeService = storeService;
+        this.workerService = workerService;
     }
 
 
@@ -75,6 +80,36 @@ public class DebugController {
         return storeService.getStores(null, null, null, null, page, size, authenticatedPrincipal.getCompanyId()).getData();
     }
 
+    // my store
+    @GetMapping("/my-store")
+    @Operation(
+            summary = "Get my store",
+            description = """
+                    This endpoint is used to get the store of the authenticated user. \s
+                    ### Authentication: bearer token is required. \s
+                    ### Authorizations: any authenticated user can get my store. \s
+                    """
+    )
+    public StoreInfoResponse getMyStore(@AuthenticationPrincipal Worker authenticatedPrincipal) {
+        return storeService.getStoreInfo(authenticatedPrincipal.getStoreId()).getData();
+    }
+
+    // my workers
+    @GetMapping("/my-workers")
+    @Operation(
+            summary = "Get my workers",
+            description = """
+                    This endpoint is used to get the workers of the authenticated user. \s
+                    ### Authentication: bearer token is required. \s
+                    ### Authorizations: any authenticated user can get my workers. \s
+                    """
+    )
+    public List<WorkerInfoResponse> getMyWorkers(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @AuthenticationPrincipal Worker authenticatedPrincipal) {
+        return workerService.getWorkers(null, null, null, null, null, authenticatedPrincipal.getCompanyId(), authenticatedPrincipal.getStoreId(), authenticatedPrincipal.getRoles().contains(Role.ROLE_ADMIN), page, size).getData();
+    }
 
 
 }
